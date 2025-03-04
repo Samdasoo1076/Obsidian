c558e7148b469c537dee96057bf15681b283f8c6
Samdasoo1076 <dlwlalsdkd@gmail.com>
2025-03-04 15:48:15 +0900

    2025-03-04

---
+++ Attached file/Connect.bat
@@ -0,0 +1,2 @@
+@echo off
+ssh -i "C:\Users\ucomp\.ssh\ssh-key-2025-03-04.key" ubuntu@140.245.76.236
---
+++ Attached file/connectSSH.bat
@@ -0,0 +1,2 @@
+@echo off
+ssh -i "C:\Users\ucomp\.ssh\ssh-key-2025-02-02.key" ubuntu@144.24.85.101
